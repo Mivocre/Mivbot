f9fe0d5ca4a92ff5b5a89162b5b61ddbd66d7589
Max Vuong <mace.max99@gmail.com>
2025-03-11 12:08:11 -0500

    added functionality and documentation in code to recognize links and attachments

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        #stops bot from replying to itself
+        if message.author == self.user:
+            return
+        if message.content.startswith("hello"):
+            await message.channel.send(f'Hi There {message.author}')
+            # this line returns a list of attachments, if no attachments present the list will be empty
+            # print(message.attachments)
+            # this line returns a list of embedded links, if no links present the list will be empty
+            # print(message.embeds)
+    
 
 intents = discord.Intents.default()
 intents.message_content = True
